2a5485f4e25d93e0a5a8fb7d157b17d96ceb06a6
Robert Hardy <rhardy@dcsdk12.org>
2022-01-11 15:58:05 -0700

    Added basic variable.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,22 @@
 /** @type {HTMLCanvasElement} */
-const canvas = document.getElementById('game-canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+console.log("Hello world!");
+
+let suit = "spades";
+let face = "queen";
+let value = 10;
+
+let isFace = true;
+
+let aceOfSpades = {
+	suit: "spades",
+	face: "ace",
+	value: 1,
+	isFace: false,
+};
+
+console.log(aceOfSpades.toString());
