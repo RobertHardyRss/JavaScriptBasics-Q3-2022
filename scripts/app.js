eaaf32342cc0ca50a7c372a3207c695dd2ffb800
Robert Hardy <rhardy@dcsdk12.org>
2022-01-12 17:48:54 -0700

    Playing with methods and properties on variables

--- scripts/app.js
+++ scripts/app.js
@@ -7,13 +7,14 @@ canvas.height = 600;
 console.log("Hello world!");
 
 // this is setting a variable suit to the string spades
-let suit = "spades";
+let suit = "spades"; // string
 let face = "queen";
-let value = 10;
+let value = 10; // number
 
-let isFace = true;
+let isFace = true; //boolean
 
 let aceOfSpades = {
+	// object
 	suit: "spades",
 	face: "ace",
 	value: 1,
@@ -31,7 +32,19 @@ multiline comment
 
 // arrays are collections of data
 
-let agesOfTable1 = [13, 23, 33];
+let agesOfTable1 = [13, 23, 33]; // array
 console.log(agesOfTable1[2]);
 
+// let myMoney = "I want lots and lots of it $$$";
+// console.log(myMoney.toUpperCase());
+// console.log("I loke mo money".length);
+
 let namesAndAges = ["Hunter", 13, "Patrick", 14, "Tanner", 14.8];
+
+namesAndAges.forEach((n) => {
+	console.log(n);
+});
+
+let over14 = agesOfTable1.filter((n) => n > 14);
+
+console.log(over14);
