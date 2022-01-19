33bc6b066185052425b883d00b1a17d1a157842e
Robert Hardy <rhardy@dcsdk12.org>
2022-01-18 17:19:25 -0700

    Add functions and an event listener

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
 
+greeting("Robert", 51);
+
 console.log("Hello world!");
 
 // this is setting a variable suit to the string spades
@@ -21,7 +23,7 @@ let aceOfSpades = {
 	isFace: false,
 };
 
-console.log(aceOfSpades.toString());
+// console.log(aceOfSpades.toString());
 
 // single line comment
 
@@ -33,7 +35,7 @@ multiline comment
 // arrays are collections of data
 
 let agesOfTable1 = [13, 23, 33]; // array
-console.log(agesOfTable1[2]);
+// console.log(agesOfTable1[2]);
 
 // let myMoney = "I want lots and lots of it $$$";
 // console.log(myMoney.toUpperCase());
@@ -42,9 +44,32 @@ console.log(agesOfTable1[2]);
 let namesAndAges = ["Hunter", 13, "Patrick", 14, "Tanner", 14.8];
 
 namesAndAges.forEach((n) => {
-	console.log(n);
+	// console.log(n);
 });
 
 let over14 = agesOfTable1.filter((n) => n > 14);
 
-console.log(over14);
+// console.log(over14);
+
+function square(number) {
+	let result = number * number;
+	return result;
+}
+
+console.log(square(2));
+
+function greeting(name, age) {
+	let ageString;
+
+	if (age > 40) {
+		ageString = " super old";
+	} else {
+		ageString = "not so old";
+	}
+
+	let sayHi = "Hi my name is " + name + " and I am " + ageString;
+
+	console.log(sayHi);
+}
+
+
--- scripts/guess.js
+++ scripts/guess.js
@@ -1,31 +1,42 @@
-let secret = Math.floor(Math.random() * 10 + 1);
-console.log(secret);
-let guesses = 0;
-let guessCount = 3;
-let playerGuess;
-let gameOver = false;
+function playGuessingGame() {
+	let secret = Math.floor(Math.random() * 10 + 1);
+	//console.log(secret);
+	let guesses = 0;
+	let guessCount = 3;
+	let playerGuess;
+	let gameOver = false;
 
-while (guesses < guessCount && !gameOver) {
-	playerGuess = Number(prompt("Pick a number between 1 and 10"));
+	while (guesses < guessCount && !gameOver) {
+		playerGuess = Number(prompt("Pick a number between 1 and 10"));
 
-	if (playerGuess === secret) {
-		// player wins
-		gameOver = true;
-		alert("You won!!");
-	} else {
-		if (playerGuess < secret) {
-			alert("Too low!");
+		if (playerGuess === secret) {
+			// player wins
+			gameOver = true;
+			alert("You won!!");
 		} else {
-			alert("Too high!");
+			if (playerGuess < secret) {
+				alert("Too low!");
+			} else {
+				alert("Too high!");
+			}
 		}
+
+		// guesses = guesses + 1;
+		// guesses += 1;
+		guesses++;
+		// ++guesses;
 	}
 
-	// guesses = guesses + 1;
-	// guesses += 1;
-	guesses++;
-	// ++guesses;
+	if (!gameOver) {
+		alert("You lose!");
+	}
 }
 
-if (!gameOver) {
-	alert("You lose!");
-}
+// listen for clicks on the button
+let guessButton = document.getElementById("guess-game");
+// let clickCount = 0;
+// console.log(guessButton);
+
+guessButton.addEventListener("click", () => {
+	playGuessingGame();
+});
