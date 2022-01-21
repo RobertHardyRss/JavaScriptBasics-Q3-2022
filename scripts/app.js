f95df046364dfd1bdacedd4532399f0366c80a15
Robert Hardy <rhardy@dcsdk12.org>
2022-01-20 17:28:19 -0700

    introduce animation with requestAnimationFrame

--- scripts/app.js
+++ scripts/app.js
@@ -6,12 +6,31 @@ canvas.height = 600;
 
 let x = 100;
 let y = 200;
+
 let width = 200;
 let height = width;
 
 ctx.fillStyle = "red";
-while (y < canvas.height) {
+
+let yDirection = 1;
+let xDirection = 1;
+
+function animate() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillRect(x, y, width, height);
-	y += 10;
-	x += 10;
+
+	if (y + height >= canvas.height) {
+		yDirection = -1;
+	}
+
+	if (x + width >= canvas.width) {
+		xDirection = -1;
+	}
+
+	y += 10 * yDirection;
+	x += 10 * xDirection;
+
+	requestAnimationFrame(animate);
 }
+
+requestAnimationFrame(animate);
