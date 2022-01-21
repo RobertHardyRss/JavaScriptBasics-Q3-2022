c09cad274a0039aaee3918b770d17c40474f6e57
Robert Hardy <rhardy@dcsdk12.org>
2022-01-21 16:26:29 -0700

    create square class

--- scripts/app.js
+++ scripts/app.js
@@ -4,31 +4,58 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
 
-let x = 100;
-let y = 200;
+class Square {
+	constructor(x, y, color) {
+		this.x = x;
+		this.y = y;
+		this.width = 200;
+		this.height = this.width;
+		this.color = color;
+		this.yDirection = 1;
+		this.xDirection = 1;
+	}
 
-let width = 200;
-let height = width;
+	update() {
+		// bounce off the bottom
+		if (this.y + this.height >= canvas.height) {
+			this.xDirection = -1;
+		}
 
-ctx.fillStyle = "red";
+		// bounce off the top
+		if (this.y <= 0) {
+			this.yDirection = 1;
+		}
 
-let yDirection = 1;
-let xDirection = 1;
+		// bounces off the right
+		if (this.x + this.width >= canvas.width) {
+			this.xDirection = -1;
+		}
+
+		// bounces off the left
+		if (this.x <= 0) {
+			this.xDirection = 1;
+		}
+		this.y += 10 * this.yDirection;
+		this.x += 10 * this.xDirection;
+	}
+
+	draw() {
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+}
+
+let square1 = new Square(100, 200, "red");
+let square2 = new Square(20, 500, "blue");
+let square2 = new Square(20, 500, "blue");
 
 function animate() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillRect(x, y, width, height);
+	square1.update();
+	square2.update();
 
-	if (y + height >= canvas.height) {
-		yDirection = -1;
-	}
-
-	if (x + width >= canvas.width) {
-		xDirection = -1;
-	}
-
-	y += 10 * yDirection;
-	x += 10 * xDirection;
+	square1.draw();
+	square2.draw();
 
 	requestAnimationFrame(animate);
 }
